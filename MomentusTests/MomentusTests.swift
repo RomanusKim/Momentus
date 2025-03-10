13b2a1987e099e6692f72f8bf50c0bd28b0a0c5b
romanus <romanusdev@gmail.com>
2025-03-10 18:11:42 +0900

    Initial Commit

---
+++ MomentusTests/MomentusTests.swift
@@ -0,0 +1,17 @@
+//
+//  MomentusTests.swift
+//  MomentusTests
+//
+//  Created by romanus on 3/10/25.
+//
+
+import Testing
+@testable import Momentus
+
+struct MomentusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
